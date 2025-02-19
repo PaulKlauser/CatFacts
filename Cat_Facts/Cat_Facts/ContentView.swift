bbdbd00b19956aa9d6bb82ff1d53e8d196e6057e
Paul Klauser <paul.klauser@gmail.com>
2025-02-19 00:12:54 -0500

    Implement iOS app to match Android functionality

--- Cat_Facts/Cat_Facts/ContentView.swift
+++ Cat_Facts/Cat_Facts/ContentView.swift
@@ -2,15 +2,39 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
-	let greet = Greeting().greet()
+    @ObservedObject private(set) var viewModel: ViewModel
 
 	var body: some View {
-		Text(greet)
+        VStack {
+            if let text = viewModel.text {
+                Text(text)
+            }
+            Button("Fetch") {
+                viewModel.fetchCatFact()
+            }
+        }
 	}
+}
+
+extension ContentView {
+    class ViewModel: ObservableObject {
+        @Published var text: String? = nil
+        func fetchCatFact() {
+            CatFactService().fetchCatFact { greeting, error in
+                DispatchQueue.main.async {
+                    if let greeting = greeting {
+                        self.text = greeting
+                    } else {
+                        self.text = error?.localizedDescription ?? "error"
+                    }
+                }
+            }
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
 	static var previews: some View {
-		ContentView()
+        ContentView(viewModel: ContentView.ViewModel())
 	}
 }
